20d4f7599c8f894df4e744f71d7255b0b1328e96
Niosco-9s-ay2526 <niosco.dy@obmontessori.edu.ph>
2026-05-12 10:59:52 +0800

    Refactor loginUser function for clarity

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 function loginUser() {
-  
     let username = document.getElementById("usernameInput").value;
     let password = document.getElementById("passwordInput").value;
-}
+
     if (username === "2526student1" && password === "12345") {
         window.alert("Login successful! Welcome, " + username + "!");
-        window.location.href = "notebook.html"; 
-
-
+        window.location.href = "notebook.html";
     } else {
         window.alert("Login failed! Please check your username and password.");
     }
-    function showHelp() {
+}
+
+function showHelp() {
     window.alert("Username = 2526student1 and Password = 12345");
 }
